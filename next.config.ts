be37c85dc850e53a1e2183cf5b49a514af3d0e62
Srikar Maddena <srikarmaddena1@gmail.com>
2026-03-02 16:49:43 -0500

    fix(build): exclude node canvas dependency from pdfjs-dist

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: "export",
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
 };
 
 export default nextConfig;
